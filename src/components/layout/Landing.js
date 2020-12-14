44809b2587155d07c15abcbcb0ce5abc70c5d3af
Artur Maizelev <arturm@securitydam.com>
2020-12-14 11:58:01 +0200

    Add p

--- src/components/layout/Landing.js
+++ src/components/layout/Landing.js
@@ -1,17 +1,24 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './Landing.scss';
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Landing.scss";
 
-const Landing = props => {
-	return (
-		<div className="landing-wrapper">
-			<h1>stop watch</h1>
-			<img src="/logo192.png" alt="stop watch" />
-			<Link className="exercise-btn" to="/start">
-				start
-			</Link>
-		</div>
-	);
+const Landing = () => {
+  return (
+    <div className="landing-wrapper">
+      <h1>stop watch</h1>
+      <img src="/logo192.png" alt="stop watch" />
+      <Link className="exercise-btn" to="/start">
+        start
+      </Link>
+
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatum
+        quod quaerat temporibus animi voluptates assumenda vitae dolores iure
+        rerum ipsum. Veritatis maxime magnam dolor vel iure quis aliquam alias
+        qui.
+      </p>
+    </div>
+  );
 };
 
 export default Landing;
